2ffa48cec4cf17a98323b38649661103654fbf4e
Sebastian <sebastian97@gmail.com>
2024-07-21 12:08:07 -0700

    Initial commit

---
+++ src/main/java/me/sebastian420/mixin/LeashableMixin.java
@@ -0,0 +1,19 @@
+package me.sebastian420.mixin;
+
+import net.minecraft.entity.Leashable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+
+@Mixin(Leashable.class)
+public interface LeashableMixin {
+
+    @ModifyConstant(
+            method = "tickLeash",
+            constant = @Constant(doubleValue = 10.0)
+    )
+    private static double modifyLeashDistance(double original) {
+        return 100.0;
+    }
+}
